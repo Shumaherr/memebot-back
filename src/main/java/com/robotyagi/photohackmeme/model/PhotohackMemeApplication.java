93c0c7d5ddf7ea9e35d8f369d59e4b038c3c6fd7
al-farabi <al-farabi@al-farabis-MacBook-Air.local>
2019-01-26 18:04:43 +0300

    post_method created

---
+++ src/main/java/com/robotyagi/photohackmeme/model/PhotohackMemeApplication.java
@@ -0,0 +1,14 @@
+package com.robotyagi.photohackmeme.model;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+public class PhotohackMemeApplication {
+
+	public static void main(String[] args) { SpringApplication.run(multipartConfigElement.class, args);}
+
+}
