dad3c66774e7ce0c1bf29aa941a4c54fdbcd9c98
Odintsov <shumaherr@gmail.com>
2019-01-26 13:32:54 +0300

    first commit

---
+++ src/main/java/com/robotyagi/photohackmeme/PhotohackMemeApplication.java
@@ -0,0 +1,14 @@
+package com.robotyagi.photohackmeme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotohackMemeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotohackMemeApplication.class, args);
+	}
+
+}
+
