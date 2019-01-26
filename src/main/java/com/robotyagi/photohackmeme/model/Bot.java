773dae3b5bfa8e6de9c415303379f6014bd89666
Odintsov <shumaherr@gmail.com>
2019-01-26 17:29:34 +0300

    Adding bot model

---
+++ src/main/java/com/robotyagi/photohackmeme/model/Bot.java
@@ -0,0 +1,58 @@
+package com.robotyagi.photohackmeme.model;
+
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Bot extends TelegramLongPollingBot {
+
+    /**
+     * Метод для приема сообщений.
+     * @param update Содержит сообщение от пользователя.
+     */
+
+    Logger log = Logger.getLogger(Bot.class.getName());
+    @Override
+    public void onUpdateReceived(Update update) {
+        String message = update.getMessage().getText();
+        sendMsg(update.getMessage().getChatId().toString(), message);
+    }
+
+    /**
+     * Метод для настройки сообщения и его отправки.
+     * @param chatId id чата
+     * @param s Строка, которую необходимот отправить в качестве сообщения.
+     */
+    public synchronized void sendMsg(String chatId, String s) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(s);
+        try {
+            sendMessage(sendMessage);
+        } catch (TelegramApiException e) {
+            log.log(Level.SEVERE, "Exception: ", e.toString());
+        }
+    }
+
+    /**
+     * Метод возвращает имя бота, указанное при регистрации.
+     * @return имя бота
+     */
+    @Override
+    public String getBotUsername() {
+        return "memefication_bot";
+    }
+
+    /**
+     * Метод возвращает token бота для связи с сервером Telegram
+     * @return token для бота
+     */
+    @Override
+    public String getBotToken() {
+        return "758153614:AAGmxQgNclxVgJV-lrs1pbHuNDQs8-J_0Ro";
+    }
+}
